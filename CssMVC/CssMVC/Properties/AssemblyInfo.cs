34ab8ac49f1c4e20ae89ed56ac936121953a33c3
buraqk1 <burakkekecc@hotmail.com>
2023-08-30 15:57:44 +0300

    flexbox on Visual Studio 2020

---
+++ CssMVC/CssMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derleme hakkındaki genel Bilgiler aşağıdakiler üzerinden denetlenir
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("CssMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CssMVC")]
+[assembly: AssemblyCopyright("Telif hakkı ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özelliğinin false olarak ayarlanması bu derlemedeki türleri COM bileşenlerine
+//COM görünmez kılar.  Bu derlemedeki bir türe şu kaynaktan erişmeniz gerekiyorsa:
+// COM, bu tür üzerinde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("10270797-4462-4a15-91d3-d89034db6df7")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya Gözden Geçirme ve Yapı Numaralarını varsayılan değerlerine döndürebilirsiniz
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
